5ff4e999e878972c2de9e04588e256047b6289b7
Maksim Pinchuk <pinchukmaksim013@gmail.com>
2025-05-28 17:45:01 +0200

    implemented queries

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ file_path_reservation = 'Updated_Reservation_csv.csv'
 with psycopg2.connect(database="BIGdata", user="postgres", password='130006') as conn:
     with conn.cursor() as cur:
         cur.execute("""
-        DROP TABLE customer, staff, menu_item, orders, payment, order_item;
+        DROP TABLE customer, staff, menu_item, orders, payment, order_item, reservation;
 
         """)
 
@@ -93,7 +93,7 @@ with psycopg2.connect(database="BIGdata", user="postgres", password='130006') as
         cur.execute("""
                    CREATE TABLE IF NOT EXISTS orders (
                        order_id INT PRIMARY KEY,
-                       order_date VARCHAR(100) NOT NULL,
+                       order_date DATE,
                        order_type VARCHAR(100) NOT NULL,
                        customer_id INT,
                        staff_id INT,
@@ -192,4 +192,104 @@ with psycopg2.connect(database="BIGdata", user="postgres", password='130006') as
         """)
 
         print(cur.fetchall())
+
+
+        # query
+        print()
+
+        # This query outputs the top employees by the number of orders in the current month.
+        print('This query outputs the top employees by the number of orders in the current month.')
+        cur.execute("""
+                    SELECT 
+                        staff.staff_id,
+                        staff.name,
+                        staff.role,
+                        COUNT(orders.order_id) AS total_orders
+                    FROM 
+                        staff
+                    JOIN 
+                        orders ON staff.staff_id = orders.staff_id
+                    WHERE 
+                        orders.order_date >= DATE_TRUNC('month', CURRENT_DATE)
+                        AND orders.order_date < DATE_TRUNC('month', CURRENT_DATE) + INTERVAL '1 month'
+                    GROUP BY 
+                        staff.staff_id, staff.name, staff.role
+                    ORDER BY 
+                        total_orders DESC;
+                """)
+
+        results_1 = cur.fetchall()
+        for row in results_1:
+            print(row)
+
+        print()
+        # This query calculate the total amount of all payments made during the year 2024.
+        print('This query calculate the total amount of all payments made during the year 2024.')
+        cur.execute("""
+            SELECT SUM(amount) FROM Payment
+            WHERE EXTRACT(YEAR FROM payment_date) = 2024;
+        """)
+
+        results_2 = cur.fetchall()
+
+        for row in results_2:
+            print(row[0])
+
+
+        print()
+        # A query to find which customers paid by card.
+        # A query to find which customers paid by cash.
+
+        print('A query to find which customers paid by card.')
+        cur.execute("""
+            SELECT COUNT(payment_id) FROM payment
+            WHERE payment_method=%s;
+            
+        """, ('Card',))
+
+        result_3 = cur.fetchall()
+
+        for row in result_3:
+            print(row[0])
+
+        print()
+        print('A query to find which customers paid by cash.')
+        cur.execute("""
+            SELECT COUNT(payment_id) FROM payment
+            WHERE payment_method=%s;
+        """, ('Cash',))
+
+        result_4 = cur.fetchall()
+        print(result_4[0][0])
+
+
+
+
+
+        print()
+        # A query to find the most popular dish based on the highest total quantity ordered.
+        print('A query to find the most popular dish based on the highest total quantity ordered.')
+        cur.execute("""
+            SELECT mi.item_name, SUM(oi.quantity) AS total_quantity
+            FROM order_item oi
+            JOIN menu_item mi ON oi.menu_item_id = mi.menu_item_id
+            GROUP BY mi.item_name
+            ORDER BY total_quantity DESC;
+        """)
+
+
+        result_5 = cur.fetchall()
+
+        dct = {row[0] : row[1] for row in result_5}
+
+        highest_quantity = result_5[0][1]
+
+        for key, value in dct.items():
+            if value == highest_quantity:
+                print(key, end=' ')
+
+
+
+
+
 
